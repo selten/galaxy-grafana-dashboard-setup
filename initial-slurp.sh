fa460d68a09d3cf7b81fc3ff13146fd7a0450cd3
selten <selten@users.noreply.github.com>
2019-07-11 11:23:07 +0200

    Create script for initial slurp
    This script does the initial slurp of data into InfluxDB so Grafana can use it

---
+++ initial-slurp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Settings for the initial slurp
+BEGINDATE=2019-01-01
+ENDDATE=2019-07-01
+INFLUXDBNAME=""
+PGDATABASE=""
+PGHOST=""
+PGUSER=""
+PGPASSWORD=""
+INFLUX_URL=""
+INFLUX_PASS=""
+INFLUX_USER=""
+
+# Settings for gxadmin
+PATH=$PATH:~/gxadmin/
+
+# create temporary file
+echo "" > /tmp/gxadmin-meta-import
+
+# Slurp the data for every date...
+d=$BEGINDATE
+while [ "$d" != $ENDDATE ]; do
+  gxadmin meta slurp-upto $d >> /tmp/gxadmin-meta-import
+
+  d=$(date -I -d "$d + 1 day")
+done
+
+# Get the current information
+#gxadmin meta slurp-current >> /tmp/gxadmin-meta-import
+
+# Post it to InfluxDB
+gxadmin meta influx-post $INFLUXDBNAME /tmp/gxadmin-meta-import
+
+# Remove the temporary file
+rm /tmp/gxadmin-meta-import
